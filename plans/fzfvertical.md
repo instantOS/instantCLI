4f0850d7443ad0fae654d8f924c998b2166d9805
paperbenni <paperbenni@gmail.com>
2025-11-02 18:47:08 +0100

    add plans

---
+++ plans/fzfvertical.md
@@ -0,0 +1,7 @@
+Extend the builder pattern for fzf to support horizontal and vertical layout
+(vertical being the default).
+
+Also, long messages with the message feature could benefit from wrapping. Look
+up if fzf can do that or if we need to manually wrap lines before passing them
+to fzf
+
--- src/game/setup/install.rs
+++ src/game/setup/install.rs
@@ -323,6 +323,7 @@ fn prepare_save_path(
 
     if kind.is_directory() {
         if !save_path.as_path().exists() {
+            //TODO save path could be pretty long, add newline after it
             match FzfWrapper::confirm(&format!(
                 "Save path '{display}' does not exist. Would you like to create it?"
             ))
