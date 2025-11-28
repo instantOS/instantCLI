69349f898dea4b3163c0a0283446f64f220be328
paperbenni <paperbenni@gmail.com>
2025-11-28 09:06:50 +0100

    fix typo

--- scripts/archinstall.sh
+++ scripts/archinstall.sh
@@ -3,7 +3,7 @@
 # VERY experimental new Rust based installer for instantOS
 
 echo "installing instantOS"
-sudo pacman -Sy --needed libgit2
+sudo pacman -Sy --needed libgit2 --noconfirm
 curl https://stuff.paperbenni.xyz/ins > ins
 chmod +x ins
 ./ins arch install
