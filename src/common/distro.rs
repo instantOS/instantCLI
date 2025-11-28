3dd08f58ec58ce89896040ef36fa5b590ec39969
paperbenni <paperbenni@gmail.com>
2025-11-28 09:34:28 +0100

    fix distro detection

--- src/common/distro.rs
+++ src/common/distro.rs
@@ -20,7 +20,19 @@ pub enum Distro {
 impl std::fmt::Display for Distro {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Distro::Arch => write!(f, "Arch Linux"),
+            Distro::Arch => {
+                // Check if we're running on instantOS specifically
+                if let Ok(content) = std::fs::read_to_string("/etc/os-release") {
+                    for line in content.lines() {
+                        if let Some(val) = line.strip_prefix("ID=") {
+                            if val.trim_matches('"') == "instantos" {
+                                return write!(f, "instantOS (Arch-based)");
+                            }
+                        }
+                    }
+                }
+                write!(f, "Arch Linux")
+            },
             Distro::Debian => write!(f, "Debian"),
             Distro::Ubuntu => write!(f, "Ubuntu"),
             Distro::Fedora => write!(f, "Fedora"),
@@ -52,11 +64,13 @@ pub fn is_live_iso() -> bool {
 
 fn parse_os_release(content: &str) -> Result<Distro> {
     let mut id = String::new();
+    let mut id_like = String::new();
 
     for line in content.lines() {
         if let Some(val) = line.strip_prefix("ID=") {
             id = val.trim_matches('"').to_string();
-            break;
+        } else if let Some(val) = line.strip_prefix("ID_LIKE=") {
+            id_like = val.trim_matches('"').to_string();
         }
     }
 
@@ -71,7 +85,22 @@ fn parse_os_release(content: &str) -> Result<Distro> {
         "endeavouros" => Ok(Distro::EndeavourOS),
         "pop" => Ok(Distro::PopOS),
         "linuxmint" => Ok(Distro::LinuxMint),
-        _ => Ok(Distro::Unknown(id)),
+        "instantos" => {
+            // instantOS is Arch-based, check ID_LIKE to confirm
+            if id_like.contains("arch") {
+                Ok(Distro::Arch)
+            } else {
+                Ok(Distro::Unknown(id))
+            }
+        },
+        _ => {
+            // For unknown IDs, check if they are Arch-based
+            if id_like.contains("arch") {
+                Ok(Distro::Arch)
+            } else {
+                Ok(Distro::Unknown(id))
+            }
+        }
     }
 }
 
@@ -109,5 +138,31 @@ BUG_REPORT_URL="https://bugs.launchpad.net/ubuntu/"
 PRIVACY_POLICY_URL="https://www.ubuntu.com/legal/terms-and-policies/privacy-policy"
 UBUNTU_CODENAME=jammy"#;
         assert_eq!(parse_os_release(content).unwrap(), Distro::Ubuntu);
+    }
+
+    #[test]
+    fn test_parse_instantos() {
+        let content = r#"NAME="instantOS"
+PRETTY_NAME="instantOS"
+ID="instantos"
+BUILD_ID=rolling
+ANSI_COLOR="38;2;23;147;209"
+HOME_URL="https://archlinux.org/"
+DOCUMENTATION_URL="https://wiki.archlinux.org/"
+SUPPORT_URL="https://bbs.archlinux.org/"
+BUG_REPORT_URL="https://gitlab.archlinux.org/groups/archlinux/-/issues"
+PRIVACY_POLICY_URL="https://terms.archlinux.org/docs/privacy-policy/"
+LOGO=archlinux-logo
+ID_LIKE="arch""#;
+        assert_eq!(parse_os_release(content).unwrap(), Distro::Arch);
+    }
+
+    #[test]
+    fn test_parse_unknown_arch_based() {
+        let content = r#"NAME="Custom Arch"
+PRETTY_NAME="Custom Arch Distro"
+ID="customarch"
+ID_LIKE="arch""#;
+        assert_eq!(parse_os_release(content).unwrap(), Distro::Arch);
     }
 }
