75dfa4054aef7e509f73ccc4f978c256c7d26863
paperbenni <paperbenni@gmail.com>
2025-12-03 13:25:34 +0100

    fix swaymsg path quoting

--- src/wallpaper/sway.rs
+++ src/wallpaper/sway.rs
@@ -3,7 +3,7 @@ use anyhow::Result;
 
 pub fn apply_wallpaper(path: &str) -> Result<()> {
     // swaymsg output "*" bg <path> fill
-    let command = format!("output \"*\" bg {} fill", path);
+    let command = format!("output \"*\" bg \"{}\" fill", path);
     sway::swaymsg(&command)?;
     Ok(())
 }
