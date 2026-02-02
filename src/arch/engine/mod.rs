025f5aadd050fea19bd72317bc944870a29fde6d
paperbenni <paperbenni@gmail.com>
2026-02-02 17:09:37 +0100

    move out previews

--- src/arch/engine/mod.rs
+++ src/arch/engine/mod.rs
@@ -1,6 +1,7 @@
 mod context;
 mod engine;
 mod question;
+mod summary;
 mod system_info;
 mod types;
 
@@ -9,4 +10,5 @@ pub use context::{
 };
 pub use engine::QuestionEngine;
 pub use question::{AsyncDataProvider, Question, QuestionResult};
+pub(crate) use summary::{InstallSummary, PartitioningKind, build_install_summary};
 pub use types::{BootMode, GpuKind, QuestionId, SystemInfo};
