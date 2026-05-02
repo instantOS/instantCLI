4b6d94b96a5020bb22ffabfb2915051446d282aa
paperbenni <paperbenni@gmail.com>
2026-05-02 17:41:46 +0200

    remove libgit2 references

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -7,9 +7,7 @@ echo "installing instantOS"
 mkdir -p /etc/instantos
 touch /etc/instantos/uploadlogs
 
-sudo pacman -Sy --needed libgit2 --noconfirm
 curl https://stuff.paperbenni.xyz/ins > /usr/local/bin/ins
 chmod +x /usr/local/bin/ins
 ins dev setup
 zsh
-
